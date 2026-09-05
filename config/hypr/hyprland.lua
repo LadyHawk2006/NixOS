239c9695e07b4922b15d4f5548f9568a92a9abb1
LadyHawk2006 <shadrackburuna@gmail.com>
2026-09-05 13:02:52 +0300

    backup: post-rebuild sync (2026-09-05 13:02:52) Host: nixos NixOS: 26.05.8954.a5cc6f2c37bf (Yarara) Files changed: 3

--- config/hypr/hyprland.lua
+++ config/hypr/hyprland.lua
@@ -41,6 +41,7 @@ hl.config({
     input = {
         kb_layout = "us",
         numlock_by_default = true,
+        kb_options = "scrolllock:mod3",
     },
 })
 
--- configuration.nix
+++ configuration.nix
@@ -48,6 +48,21 @@ in
 
 hardware.bluetooth.enable = true;
 hardware.xone.enable = true; # Adds enhanced Xbox controller drivers/rules
+services.actkbd.enable = true;
+services.actkbd.bindings = [
+  # Mute
+  { keys = [ 113 ]; events = [ "key" ];
+    command = "${pkgs.alsa-utils}/bin/amixer -q set Master toggle"; }
+  # Volume down
+  { keys = [ 114 ]; events = [ "key" "rep" ];
+    command = "${pkgs.alsa-utils}/bin/amixer -q set Master 1- unmute"; }
+  # Volume up
+  { keys = [ 115 ]; events = [ "key" "rep" ];
+    command = "${pkgs.alsa-utils}/bin/amixer -q set Master 1+ unmute"; }
+  # Mic Mute
+  { keys = [ 190 ]; events = [ "key" ];
+    command = "${pkgs.alsa-utils}/bin/amixer -q set Capture toggle"; }
+];
 
 
 services.blueman.enable = true;
@@ -98,6 +113,7 @@ xdg.portal = {
     vscode
     kdePackages.kwallet
     kdePackages.dolphin
+    wev
     usbutils
     fastfetch
     gnome-software
