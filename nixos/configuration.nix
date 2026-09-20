eaaa5eae17e1841c887f8362a13848f92f2f8995
LadyHawk2006 <shadrackburuna@gmail.com>
2026-09-20 09:48:34 +0300

    backup: post-rebuild sync 2026-09-20 09:48:34 Host: nixos NixOS: 26.11pre1076192.20b1ddd1aa5a (Zokor) Files changed: 1

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -24,6 +24,7 @@ in
   nixpkgs.config.allowUnfree = true;
 
   nix.settings = {
+    experimental-features = [ "nix-command" "flakes" ];
     auto-optimise-store = true;
     substituters = [ "https://attic.xuyh0120.win/lantian" ];
     trusted-public-keys = [ "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc=" ];
